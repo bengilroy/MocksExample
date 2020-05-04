9694450ea5af2e69778c182fdea1f40c267aee3e
Ben Gilroy <ben.gilroy@theappbusiness.com>
2020-05-04 17:11:42 +0100

    Initial Commit

---
+++ MocksExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MocksExample
+//
+//  Created by Ben Gilroy on 04/05/2020.
+//  Copyright © 2020 The App Business. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
